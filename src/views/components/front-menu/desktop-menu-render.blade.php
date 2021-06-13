20126be178f529f5eb71846cbe704f4d6dc52b74
Sina <sina.rajabpour@outlook.com>
2021-06-13 16:09:55 +0430

    Added side-menu class to desktop front menu and handled hrading menus

--- src/views/components/front-menu/desktop-menu-render.blade.php
+++ src/views/components/front-menu/desktop-menu-render.blade.php
@@ -2,14 +2,14 @@
     @php
 
     @endphp
-    <li class="{{ count($menu->active_menu_items) > 0 ? 'menu_dropdown' : '' }} {{ is_null($menu->css_class) ? '' : $menu->css_class }}">
+    <li class="side_menu menu-item">
         <a href="{{ is_null($menu->url) ? '#' : $menu->url }}">
             {{ $menu->title }}
         </a>
         @if(count($menu->active_menu_items) > 0)
             <ul class="header-main-menu-1st-child">
                 @foreach($menu->active_menu_items as $active_menu_item)
-                    <li class="menu-item">
+                    <li class="menu-item {{ is_null($active_menu_item->css_class) ? '' : $active_menu_item->css_class }}">
                         <a href="{{ is_null($active_menu_item->url) ? '#' : $active_menu_item->url }}">
                             {{ $active_menu_item->title }}
                         </a>
@@ -17,14 +17,28 @@
                             <div class="row">
                                 <div class="mega-menu">
                                     <div class="menu-item">
-                                        <ul class="end-menu">
-                                            @foreach($active_menu_item->children as $child)
-                                                <li class="menu-item">
-                                                    <a href="{{ is_null($child->url) ? '#' : $child->url }}">
-                                                        <span class="fa fa-angle-left fa-fw"></span>{{ $child->title }}</a>
-                                                </li>
-                                            @endforeach
-                                        </ul>
+                                        @foreach($active_menu_item->children as $child)
+                                            <ul class="end-menu">
+                                                @if($child->type == 'heading')
+                                                    <li>
+                                                        <h5>{{ $child->title }}</h5>
+                                                    </li>
+                                                @else
+                                                    <li class="menu-item">
+                                                        <a href="{{ is_null($child->url) ? '#' : $child->url }}">
+                                                            <span class="fa fa-angle-left fa-fw"></span>{{ $child->title }}
+                                                        </a>
+                                                    </li>
+                                                @endif
+                                                @foreach($child->children as $item)
+                                                    <li class="menu-item">
+                                                        <a href="{{ is_null($item->url) ? '#' : $item->url }}">
+                                                            <span class="fa fa-angle-left fa-fw"></span>{{ $item->title }}
+                                                        </a>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                        @endforeach
                                     </div>
                                 </div>
                             </div>
