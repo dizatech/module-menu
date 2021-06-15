f4ea35b2b43f3325c79f87939cf62b822a8fde3e
Sina <sina.rajabpour@outlook.com>
2021-06-15 17:56:42 +0430

    Fixed wrong parent-title in menu items list

--- src/Http/Controllers/MenuItemController.php
+++ src/Http/Controllers/MenuItemController.php
@@ -88,7 +88,7 @@ class MenuItemController extends Controller
                 'status' => '200',
                 'id' => $menu->id,
                 'title' => $menu->title,
-                'parent_title' => $menu->parent == null ? '' : $menu->parent->title,
+                'parent_title' => $menu->parent == null ? '-' : $menu->parent->title,
                 'status_label' => $menu->status_label,
                 'type_label' => $menu->type_label,
                 'message' => 'آیتم باموفقیت ثبت شد.',
